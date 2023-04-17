21d6d15a718ab6e1237415d856d408be0d26f69b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-04-18 00:08:04 +0300

    chore: init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
+  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+  outputs = inputs @ {
+    parts,
+    nci,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [nci.flakeModule];
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: let
+        crateName = "musikquadrupled";
+        crateOutputs = config.nci.outputs.${crateName};
+      in {
+        nci.projects.${crateName}.relPath = "";
+        nci.crates.${crateName} = {
+          export = true;
+        };
+        devShells.default = crateOutputs.devShell;
+        packages.default = crateOutputs.packages.release;
+      };
+    };
+}
